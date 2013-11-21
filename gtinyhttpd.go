40bdaa17a9b82490c2b8e6f11ed6a563550a2d17
mat2uken <mat2uken@localhost>
2013-11-21 23:19:42 +0900

    cleaning by gofmt

--- gtinyhttpd.go
+++ gtinyhttpd.go
@@ -9,15 +9,15 @@ import "fmt"
 func main() {
 	var path *string = flag.String("path", ".", "serving files dir path.")
 	var port *int = flag.Int("port", 8080, "serving port.")
-    flag.Parse()
+	flag.Parse()
 
-    var apath, err = filepath.Abs(*path)
-    if err != nil {
-    	log.Println("Error:", err)
-    }
-    log.Printf("Start Serving HTTP => directory: %s, port: %d", apath, *port)
+	var apath, err = filepath.Abs(*path)
+	if err != nil {
+		log.Println("Error:", err)
+	}
+	log.Printf("Start Serving HTTP => directory: %s, port: %d", apath, *port)
 
-    var server = http.StripPrefix("/", http.FileServer(http.Dir(*path)))
+	var server = http.StripPrefix("/", http.FileServer(http.Dir(*path)))
 	http.Handle("/", server)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
 		log.Println("ListenAndServe:", err)
