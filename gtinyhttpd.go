d773613249efedd994d80c046965ff7dea534c94
mat2uken <mat2uken@localhost>
2013-11-21 22:36:45 +0900

    initial commit

---
+++ gtinyhttpd.go
@@ -0,0 +1,25 @@
+package main
+
+import "net/http"
+import "flag"
+import "log"
+import "path/filepath"
+import "fmt"
+
+func main() {
+	var path *string = flag.String("path", ".", "serving files dir path.")
+	var port *int = flag.Int("port", 8080, "serving port.")
+    flag.Parse()
+
+    var apath, err = filepath.Abs(*path)
+    if err != nil {
+    	log.Println("Error:", err)
+    }
+    log.Printf("Start Serving HTTP => directory: %s, port: %d", apath, *port)
+
+    var server = http.StripPrefix("/", http.FileServer(http.Dir(*path)))
+	http.Handle("/", server)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
+		log.Println("ListenAndServe:", err)
+	}
+}
