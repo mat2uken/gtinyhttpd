8919215a184946a10d11d109749353237841c3a6
mat2uken <mat2uken@localhost>
2013-11-25 16:39:57 +0900

    /etc/hosts????????HTTPS????????????????
    ????q???????????
    Ctrl-C???????/etc/hosts????????

--- src/bitbucket.org/mat2uken/gtinyhttpd/gtinyhttpd.go
+++ src/bitbucket.org/mat2uken/gtinyhttpd/gtinyhttpd.go
@@ -7,6 +7,7 @@ import "path/filepath"
 import "fmt"
 import "os"
 import "os/exec"
+import "os/signal"
 import "io/ioutil"
 import "bufio"
 import "strings"
@@ -17,13 +18,82 @@ const (
 	HostFilePath = "/private/etc/hosts"
 )
 
-func ReadHostsFile() {
+type EditHostFileHandlerFunc func(entry []string) []string
 
+func EditHostsFile(edit_func EditHostFileHandlerFunc) {
+	var lines []string
+	f, err := os.Open(HostFilePath)
+	if err != nil {
+		panic(err)
+	}
+	r := bufio.NewScanner(f)
+	for r.Scan() {
+		line := r.Text()
+
+		// split by whitespaces
+		entry := strings.Fields(line)
+
+		if strings.Index(entry[0], "#") == 0 {
+			lines = append(lines, line)
+			continue
+		}
+		if entry[0] != "127.0.0.1" {
+			lines = append(lines, line)
+			continue
+		}
+
+		lines = append(lines, strings.Join(edit_func(entry), " "))
+	}
+
+	content := []byte(strings.Join(lines, "\n") + "\n")
+	if err := ioutil.WriteFile(HostFilePath, content, 0644); err != nil {
+		panic(err)
+	}
+}
+
+func AddLocalHostNameToHostsFile(hostname string) {
+	edit_func := func(entry []string) []string {
+		for _, v := range entry {
+			if v == hostname {
+				return entry
+			}
+		}
+		return append(entry, hostname)
+	}
+	EditHostsFile(edit_func)
+}
+
+func RemoveLocalHostNameFromHostsFile(hostname string) {
+	edit_func := func(entry []string) []string {
+		tmp_entry := entry
+		for i, v := range tmp_entry {
+			if v == hostname {
+				entry = append(entry[:i], entry[i+1:]...)
+			}
+		}
+		return entry
+	}
+	EditHostsFile(edit_func)
+}
+
+func ClearDNSCache() {
+	// clear DNS Cache by OS layer if exists
+	switch runtime.GOOS {
+	case "darwin":
+		if err := exec.Command("/usr/bin/dscacheutil", "-flushcache").Run(); err != nil {
+			log.Fatalf("cannnot run command: dscacheutil -flushcache => %v", err)
+		}
+		//			out, oerr := exec.Command("/usr/bin/killall", "-HUP", "mDNSResponder").Output()
+		//			log.Printf("out: %v, err:%v", out, oerr)
+		if err := exec.Command("/usr/bin/killall", "-HUP", "mDNSResponder").Run(); err != nil {
+			log.Fatalf("cannot run command: killall -HUP mDNSResponder => %v", err)
+		}
+	}
 }
 
 func main() {
-	var add_hosts *string = flag.String("add-hosts", "", "add entry to hosts.")
-	var del_hosts *string = flag.String("del-hosts", "", "del entry to hosts.")
+	var add_hostname *string = flag.String("add-hosts", "", "add entry to hosts.")
+	var del_hostname *string = flag.String("del-hosts", "", "del entry to hosts.")
 	var path *string = flag.String("path", ".", "serving files dir path.")
 	var http_port *int = flag.Int("http_port", 8080, "serving port for http.")
 	var https_port *int = flag.Int("https_port", 8443, "serving port for http.")
@@ -32,67 +102,21 @@ func main() {
 	var ssl_key_file_path *string = flag.String("ssl-key", "", "ssl certificate key file.")
 	flag.Parse()
 
-	if *add_hosts != "" || *del_hosts != "" {
-		var uid = os.Getuid()
+	if *add_hostname != "" || *del_hostname != "" {
+		uid := os.Getuid()
 		if uid != 0 {
 			log.Printf("are you root? => uid: %d", uid)
 			os.Exit(1)
 		}
 
-		var lines []string
-		f, err := os.Open(HostFilePath)
-		if err != nil {
-			panic(err)
+		if *add_hostname != "" {
+			AddLocalHostNameToHostsFile(*add_hostname)
 		}
-		r := bufio.NewScanner(f)
-		for r.Scan() {
-			line := r.Text()
-
-			// skip: comment out line
-			if strings.Index(line, "#") == 0 {
-				lines = append(lines, line)
-				continue
-			}
-
-			// split by whitespaces
-			entry := strings.Fields(line)
-			if entry[0] != "127.0.0.1" {
-				lines = append(lines, line)
-				continue
-			}
-
-			if *add_hosts != "" {
-				line = strings.Join(append(entry, *add_hosts), " ")
-			}
-			if *del_hosts != "" {
-				tmp_entry := entry
-				for i, v := range tmp_entry {
-					if v == *del_hosts {
-						entry = append(entry[:i], entry[i+1:]...)
-					}
-				}
-				line = strings.Join(entry, " ")
-			}
-			lines = append(lines, line)
+		if *del_hostname != "" {
+			RemoveLocalHostNameFromHostsFile(*del_hostname)
 		}
 
-		content := []byte(strings.Join(lines, "\n") + "\n")
-		if err := ioutil.WriteFile(HostFilePath, content, 0644); err != nil {
-			panic(err)
-		}
-
-		// clear DNS Cache by OS layer if exists
-		switch runtime.GOOS {
-		case "darwin":
-			if err := exec.Command("/usr/bin/dscacheutil", "-flushcache").Run(); err != nil {
-				log.Fatalf("cannnot run command: dscacheutil -flushcache => %v", err)
-			}
-			//			out, oerr := exec.Command("/usr/bin/killall", "-HUP", "mDNSResponder").Output()
-			//			log.Printf("out: %v, err:%v", out, oerr)
-			if err := exec.Command("/usr/bin/killall", "-HUP", "mDNSResponder").Run(); err != nil {
-				log.Fatalf("cannot run command: killall -HUP mDNSResponder => %v", err)
-			}
-		}
+		ClearDNSCache()
 
 		os.Exit(0)
 	}
@@ -104,23 +128,44 @@ func main() {
 	var server = http.StripPrefix("/", http.FileServer(http.Dir(*path)))
 	http.Handle("/", server)
 
+	log.Printf("Start Serving HTTP => directory: %s, http_port: %d", apath, *http_port)
 	go func() {
-		log.Printf("Start Serving HTTP => directory: %s, http_port: %d", apath, *http_port)
 		if err := http.ListenAndServe(fmt.Sprintf(":%d", *http_port), nil); err != nil {
 			log.Fatalf("cannot listen http: port=>%d", *http_port)
-			panic(err)
+			return
 		}
 	}()
 
 	if *ssl_cert_file_path != "" && *ssl_key_file_path != "" {
+		if *ssl_hostname == "" {
+			log.Fatal("You must specify hostname to enable ssl certificate.")
+			os.Exit(1)
+		}
+
+		log.Printf("Adding ssl-hostname to hosts file: hostname=>%v", *ssl_hostname)
+		AddLocalHostNameToHostsFile(*ssl_hostname)
+		ClearDNSCache()
+
 		log.Printf("Start Serving HTTPS => directory: %s, https_port: %d", apath, *https_port)
 		go func() {
 			if err := http.ListenAndServeTLS(fmt.Sprintf(":%d", *https_port), *ssl_cert_file_path, *ssl_key_file_path, nil); err != nil {
 				log.Fatalf("cannot listen https: port=>%d", *https_port)
-				panic(err)
+				RemoveLocalHostNameFromHostsFile(*ssl_hostname)
+				return
 			}
 		}()
 	}
+
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, os.Interrupt)
+		s := <-c
+		RemoveLocalHostNameFromHostsFile(*ssl_hostname)
+		log.Printf("Remove hosts file entry: ssl_hostname => %v", *ssl_hostname)
+
+		log.Printf("Exiting with %v", s)
+		os.Exit(0)
+	}()
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
@@ -128,6 +173,19 @@ func main() {
 		if err != nil {
 			break
 		}
-		log.Printf("input line: %v", line)
+		log.Printf("line: %v", line)
+		switch line[:1] {
+		case "a":
+			AddLocalHostNameToHostsFile(*ssl_hostname)
+			log.Printf("Added hosts file entry: ssl_hostname => %v", *ssl_hostname)
+		case "r":
+			RemoveLocalHostNameFromHostsFile(*ssl_hostname)
+			log.Printf("Remove hosts file entry: ssl_hostname => %v", *ssl_hostname)
+		case "q":
+			RemoveLocalHostNameFromHostsFile(*ssl_hostname)
+			log.Printf("Remove hosts file entry: ssl_hostname => %v", *ssl_hostname)
+			log.Printf("Exiting...")
+			os.Exit(0)
+		}
 	}
 }
